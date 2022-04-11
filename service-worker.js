eb536516df686723904f50c935983489e21ecb8b
clebsonsantos <clebsonsantos@users.noreply.github.com>
2022-04-11 19:43:54 +0000

    deploy: ecf69257b4ad5107a9c069bc42a5366244d54978

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2d9a153dc5a89babca3d75ddae29be8c.js"
+  "github-page/precache-manifest.afa99ad4dc54c0058f93cdeb0961bd2b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("github-page/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
